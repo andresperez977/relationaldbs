4636dfe4c1ef4afe9d71c1e5f881f85612f2c4fd
Andres <andresperezperales@gmail.com>
2026-01-22 10:55:02 +0100

    base de datos

---
+++ lib/src/main/java/relationaldbs/test/DBConnectionTest.java
@@ -0,0 +1,65 @@
+package relationaldbs.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBConnectionTest {
+
+	private final static String postgresqlURL = "jdbc:postgresql://localhost:5432/postgres";
+	// "jdbc:postgresql://192.168.1.170.5432/sample?ssl=true";
+	private static String username = "postgres";
+	private static String password = "Admin";
+
+	public static void main(String[] args) {
+		try {
+			Connection conn = DriverManager.getConnection(postgresqlURL, username, password);
+			System.out.println("the address of the connection object is" + conn);
+
+			createDatabase(conn);
+			String dropTableSQL = "drop table if exists users";
+			PreparedStatement ps=
+					conn.prepareStatement(dropTableSQL);
+			ps.executeUpdate();
+			ps.close();
+			// table creation sql
+			String createTableSQL = "create table if not exists users(" + "id integer not null,"
+					+ "usenname VARCHAR(255), " + "psw VARCHAR(255)," + "isVIP boolean," + "balance numeric,"
+					+ "PRIMARY KEY (id)" + ")";
+			PreparedStatement ps1 = conn.prepareStatement(createTableSQL);
+			ps1.executeUpdate();
+			ps1.close();
+			// insert sal
+			String insertSQL = "insert into users(id,usenname,psw, isVIP, balance) values ('18','Andres',1243,true,234.4),('20','Alejandro','123',false,234.3)";
+			
+			PreparedStatement ps11 = conn.prepareStatement(insertSQL);
+			System.out.println(ps11.executeUpdate());
+			ps11.close();
+			// delete sal
+			String deleteSQL = "DELETE FROM users WHERE usenname = 'Alejandro'";
+			// select psw, isVIP from users where username = 'Manolo' ;
+			PreparedStatement ps2 = conn.prepareStatement(deleteSQL);
+			System.out.println(ps2.executeUpdate());
+			ps11.close();
+			String selectSQL = "select * from users where usenname = 'Manolo";
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void createDatabase(Connection conn) {
+		// TODO Auto-generated method stub
+		try {
+			String dbCreationSQL = "CREATE DATABASE  happylearning";
+			PreparedStatement ps = conn.prepareStatement(dbCreationSQL);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
+}
