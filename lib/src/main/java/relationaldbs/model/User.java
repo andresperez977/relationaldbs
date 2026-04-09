48eb994b50d4bdac306307998bc14164ae2ff49c
Andres <andresperezperales@gmail.com>
2026-04-09 10:51:27 +0200

    crear la clase producto

---
+++ lib/src/main/java/relationaldbs/model/User.java
@@ -0,0 +1,98 @@
+package relationaldbs.model;
+
+public class User {
+
+	//field 
+	private Long id;
+	private String name;
+	private String password;
+	private double balance;
+	private String email;
+	private int phone;
+	private String age;
+	private String gender;
+	private String city;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+		
+	}
+	public User(Long id, String name, String password, double balance, String email,Integer phone, String age,String gender,String city) {
+		this.id=id;
+		this.name = name;
+		this.password = password;
+		this.balance = balance;
+		this.email= email;
+		this.phone= phone;
+		this.age= age;
+		this.gender= gender;
+		this.city=city;
+	}
+}
--- lib/src/test/java/relationaldbs/LoggingTest.java
+++ lib/src/test/java/relationaldbs/LoggingTest.java
@@ -1,17 +1,88 @@
 package relationaldbs;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
 
+import ch.qos.logback.classic.Logger;
 
 public class LoggingTest {
-public static void main(String[] args) {
-	 Logger logger= LoggerFactory.getLogger(LoggingTest.class);
-	 logger.trace(null);
-	 logger.debug(null);
-	 logger.info("");
-	 logger.warn(null);
-	 logger.error(null);
-}
+
+	/*
+	 * 
+	 * static members of a class have only one copy in the memory no matter
+	 * 
+	 * how many objects of the class we have created
+	 * 
+	 * a final variable's value can not be modified
+	 */
+
+	private final static Logger logger = (Logger) LoggerFactory.getLogger(LoggingTest.class);
+
+	public static void main(String[] args) {
+
+		logger.setLevel(Level.TRACE);
+
+//		logger.trace(null);
+
+//		logger.debug(null);
+
+//		logger.info("");
+
+//		logger.warn(null);
+
+//		logger.error(null);
+
+//		
+
+		division(20, 0);
+
+	}
+
+	private static void division(int i, int j) {
+
+		// this level of information
+
+		logger.trace("trace info-vey detailed info: you have entered the method division");
+
+		// we use this level of information for test
+
+		logger.debug("test info: division method test");
+
+		// we use this level of log for normal and general information
+
+		logger.info("main general info to show our app is running normally");
+
+		// we use this level of log for potential problems
+
+		logger.warn("unexpected problems but not vital");
+
+		// this is for errors
+
+		logger.error("serious problem that should be resolved");
+
+		if (j == 0) {
+
+			logger.info("the user has passed a zero as a dividend");
+
+			return;
+
+		}
+
+		try {
+
+			int result = i / j;
+
+			logger.info("the result is : " + result);
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 }
