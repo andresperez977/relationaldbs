0fda70994c51f20eab6c646ea67c94d597ee77af
Andres <andresperezperales@gmail.com>
2026-04-14 10:17:40 +0200

    imple userdao

--- lib/src/main/java/relationaldbs/dao/UserDao.java
+++ lib/src/main/java/relationaldbs/dao/UserDao.java
@@ -1,5 +1,24 @@
 package relationaldbs.dao;
 
-public class UserDao {
 
+import java.util.List;
+
+import relationaldbs.model.User;
+
+public interface UserDao {
+
+	public boolean insert(User user);
+	
+	
+	public boolean delete(User user);
+	
+	
+	public boolean update(User user);
+	
+	
+	public User find(Long id);
+	
+	public List<User> findall();
+	
+	
 }
